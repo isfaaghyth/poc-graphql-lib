a9919a1c2c293bb3f8841e7a7a118a0ebe8ead89
isfaaghyth <isfaaghyth@gmail.com>
2020-08-10 00:57:40 +0700

    renamed package properties

--- src/main/java/gql/entity/QueryParams.kt
+++ src/main/java/gql/entity/QueryParams.kt
@@ -1,4 +1,4 @@
-package query
+package gql.entity
 
 data class QueryParams(
     val variable: String,
--- src/test/java/gql/QueryParameterParserTest.kt
+++ src/test/java/gql/QueryParameterParserTest.kt
@@ -3,8 +3,8 @@ package gql
 import gql.util.FileUtil
 import org.junit.Assert
 import org.junit.Test
-import query.QueryParams
-import query.QueryParameterParser
+import gql.entity.QueryParams
+import gql.query.QueryParameterParser
 
 class QueryParameterParserTest {
 
@@ -18,10 +18,24 @@ class QueryParameterParserTest {
     }
 
     @Test fun `should return param1 as variable and String as data type of param1`() {
-        val expectedValue = arrayOf(QueryParams("param1", "String"))
+        val expectedValue = arrayOf(QueryParams("param1", "java.lang.String"))
         val result = QueryParameterParser.parameters(query)
+        println(result)
         Assert.assertEquals(result.first().variable, expectedValue.first().variable)
         Assert.assertEquals(result.first().type, expectedValue.first().type)
+    }
+
+    @Test fun `validate variable data type with java reflection`() {
+        val string = Class.forName("java.lang.String")
+        val integer = Class.forName("java.lang.Integer")
+        val boolean = Class.forName("java.lang.Boolean")
+        Assert.assertEquals(string.kotlin, String::class)
+        Assert.assertEquals(integer.kotlin, Int::class)
+        Assert.assertEquals(boolean.kotlin, Boolean::class)
+    }
+
+    @Test fun `verified variable data type`() {
+        assert(1.javaClass == Int::class.java)
     }
 
 }
